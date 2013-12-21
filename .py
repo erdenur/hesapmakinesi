61fbdf8ec153b8d8f7ab36e3d792374ac921a413
erdenur <erdenur@windowslive.com>
2013-12-22 00:02:06 +0200

    hesapmakinesi.py
    j='1'
    while j == '1':
        soru = input("yapilacak islemin numarasini giriniz: ")
        if soru == '1':
            x=input("sayi1: ")
            y=input("sayi2: ")
            print (int(x)+int(y))
            j=input("yeni islem yapmak icin 1'e,cikmak icin 0'a basip;ardindan enter'a basiniz: ")
        elif soru == '2':
            x=input("sayi1: ")
            y=input("sayi2: ")
            print (int(x)-int(y))
            j=input("yeni islem yapmak icin 1'e,cikmak icin 0'a basip;ardindan enter'a basiniz: ")
        elif soru == '3':
            x=input("sayi1: ")
            y=input("sayi2: ")
            print (int(x)*int(y))
            j=input("yeni islem yapmak icin 1'e,cikmak icin 0'a basip;ardindan enter'a basiniz: ")
        elif soru == '4':
            x=input("sayi1: ")
            y=input("sayi2: ")
            print (int(x)/int(y))
            j=input("yeni islem yapmak icin 1'e,cikmak icin 0'a basip;ardindan enter'a basiniz: ")
        elif soru == '5':
            x=input("hangi sayinin üssünü alacaksiniz?: ")
            y=input("sayinin hangi üssünü alacaksiniz?: ")
            print (int(x)**int(y))
            j=input("yeni islem yapmak icin 1'e,cikmak icin 0'a basip;ardindan enter'a basiniz: ")

---
+++ .py
@@ -0,0 +1,34 @@
+j='1'
+while j == '1':
+    soru = input("yapilacak islemin numarasini giriniz: ")
+    if soru == '1':
+        x=input("sayi1: ")
+        y=input("sayi2: ")
+        print (int(x)+int(y))
+        j=input("yeni islem yapmak icin 1'e,cikmak icin 0'a basip;ardindan enter'a basiniz: ")
+    elif soru == '2':
+        x=input("sayi1: ")
+        y=input("sayi2: ")
+        print (int(x)-int(y))
+        j=input("yeni islem yapmak icin 1'e,cikmak icin 0'a basip;ardindan enter'a basiniz: ")
+    elif soru == '3':
+        x=input("sayi1: ")
+        y=input("sayi2: ")
+        print (int(x)*int(y))
+        j=input("yeni islem yapmak icin 1'e,cikmak icin 0'a basip;ardindan enter'a basiniz: ")
+    elif soru == '4':
+        x=input("sayi1: ")
+        y=input("sayi2: ")
+        print (int(x)/int(y))
+        j=input("yeni islem yapmak icin 1'e,cikmak icin 0'a basip;ardindan enter'a basiniz: ")
+    elif soru == '5':
+        x=input("hangi sayinin üssünü alacaksiniz?: ")
+        y=input("sayinin hangi üssünü alacaksiniz?: ")
+        print (int(x)**int(y))
+        j=input("yeni islem yapmak icin 1'e,cikmak icin 0'a basip;ardindan enter'a basiniz: ")
+
+        
+
+
+    
+    
